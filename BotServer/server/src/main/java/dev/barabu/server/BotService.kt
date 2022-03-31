44b9c90e3b6309c2778d450a4f7dcb0b07623604
Sergey Yarlykov <siyarlykov@mts.ru>
2022-03-31 07:29:18 +0300

    change listeners repo

--- BotServer/server/src/main/java/dev/barabu/server/BotService.kt
+++ BotServer/server/src/main/java/dev/barabu/server/BotService.kt
@@ -4,24 +4,68 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.os.Process
+import android.os.RemoteCallbackList
 import android.util.Log
 import dev.barabu.server.data.IOnPersonListener
 import dev.barabu.server.data.Person
 import java.util.concurrent.CopyOnWriteArrayList
 import kotlin.random.Random
 
+// И Proxy и Stub имплементят IMyAidlInterface, но делают это по разному. Proxy конвертирует
+// вызовы методов в транзакции, которые потом уходят через ядро в Stub. А Stub конвертирует
+// транзакции в вызов методов IMyAidlInterface, которые выполняют реальную работу на стороне
+// сервера.
+//
+// Биндер - это любой объект, который унаследовал от классса Binder (например IMyAidlInterface.Stub)
+// Binder Token - это уникальный 32-х битный ID биндера в системе. Обычный Binder не публикуется
+// в системе через Service Manager. Его Token известен только подключившемуся клиенту.
+
+//
+//                         --------------------
+//                         .                  .                    -----------
+//                         . IMyAidlInterface .                    .         .
+//              .--------> .                  . <---.        .---> .  Binder . --> IBinder
+//              |          --------------------     |        |     .         .
+//              |                                   |        |     -----------
+//    .---------'--------------                     |--------|
+//    .  Proxy                .                     .        .
+//    .  -----                .                     .  Stub  .
+//    .  val mRemote: Binder  .                     .        .
+//    -------------------------                     ----------
+//
+//   В Proxy методы IMyAidlInterface делают:
+//
+//   mRemote.transact(.....)                 -->    onTransact (op_code, in_buffer, out_buffer, ..) {
+//                      |                    |        switch(op_code) {
+//                      |                    |          вызовы функций из IMyAidlInterface
+//                      '------> KERNEL -----'        }
+//                                                 }
+
 /**
  * Запуск сервиса в отдельном процессе:
  * https://developer.android.com/guide/topics/manifest/service-element#proc
  * https://medium.com/@rotxed/going-multiprocess-on-android-52975ed8863c
  * https://kelvinma.medium.com/exploring-android-processes-bf74ba63552c
+ *
+ * Важная инфа про Binder Tokens:
+ * https://www.androiddesignpatterns.com/2013/07/binders-window-tokens.html
+ * https://stackoverflow.com/a/33292184
+ *
+ * Вот тут пример того как реализовать два биндера в однм сервисе
+ * https://android.googlesource.com/platform/development/+/master/samples/ApiDemos/src/com/example/android/apis/app/RemoteService.java
  */
 class BotService : Service() {
 
     /**
      * A thread-safe variant of java.util.ArrayLis
      */
-    private val personListeners = CopyOnWriteArrayList<IOnPersonListener>()
+    /*private val personListeners = CopyOnWriteArrayList<IOnPersonListener>()*/
+
+    /**
+     * Лучше пользоваться этим классом, потому что он автоматически поддерживает связь с
+     * биндерами клиентов (биндеры колбеков) и отслеживает link to death.
+     */
+    private val personListeners = RemoteCallbackList<IOnPersonListener>()
 
     private val personRepo = listOf(
         Person("Mike", "Green", 25),
@@ -70,19 +114,22 @@ class BotService : Service() {
         override fun loadPerson() {
             Log.d(LOG_TAG, "loadPerson thread ${Thread.currentThread().name}")
             Thread.sleep(Random.nextLong(2, 5) * 1000)
+
+            val listenersQty = personListeners.beginBroadcast()
+
             with(fetchPerson()) {
-                personListeners.forEach { it.onPersonLoaded(this) }
+                for (i in 0 until listenersQty) {
+                    personListeners.getBroadcastItem(i).onPersonLoaded(this)
+                }
             }
         }
 
-        override fun registerListener(listener: IOnPersonListener) {
-            if (!personListeners.contains(listener)) {
-                personListeners.add(listener)
-            }
+        override fun registerListener(listener: IOnPersonListener?) {
+            listener?.let(personListeners::register)
         }
 
-        override fun unregisterListener(listener: IOnPersonListener) {
-            personListeners.remove(listener)
+        override fun unregisterListener(listener: IOnPersonListener?) {
+            listener?.let(personListeners::unregister)
         }
     }
 
